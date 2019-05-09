6ff6b9976f3ec384fb62cc2727dc2f99f688e10a
Dan-Vy Le <danvyle85@gmail.com>
2019-05-09 22:59:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,7 +39,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"  
-  repeats = 1..number_of_times
+  repeats = 0..number_of_times
     for number in number_of_times 
       print phrase
     end
